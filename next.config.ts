282c762be57d28ae8ff1c0006e4bbc413581e9a5
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-05-03 17:05:58 +0600

    chore: update allowedDevOrigins to accept all origins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.10.7.47'],
+  allowedDevOrigins: ["*"],
   images: {
     remotePatterns: [
       {
@@ -11,7 +11,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-
 };
 
 export default nextConfig;
